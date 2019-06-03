845c9d552c8868e3b32b27631f5d4804066e18e4
Sara3108 <51201757+Sara3108@users.noreply.github.com>
2019-06-03 12:46:46 +0200

    place-details edits

--- src/app/Components/place-details/place-details.component.ts
+++ src/app/Components/place-details/place-details.component.ts
@@ -1,9 +1,11 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit ,ViewEncapsulation} from '@angular/core';
 
 @Component({
   selector: 'app-place-details',
   templateUrl: './place-details.component.html',
-  styleUrls: ['./place-details.component.css']
+  styleUrls: ['./place-details.component.css'],
+  encapsulation: ViewEncapsulation.None
+  
 })
 export class PlaceDetailsComponent implements OnInit {
 
