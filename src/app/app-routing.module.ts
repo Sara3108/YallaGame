96239060c1aa4e1cae3b34902f6017a34fe16642
Badr Shahin <badr.shahin2014@gmail.com>
2019-06-23 12:42:20 +0200

    modifiy app route module

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,19 +8,24 @@ import { PlaceDetailsComponent } from './Components/place-details/place-details.
 import { InvitationComponent } from './Components/invitation/invitation.component';
 import { UserProfileComponent } from './Components/user-profile/user-profile.component';
 import { ContinuedRegisterComponent } from './Components/continued-register/continued-register.component';
+import { WelcomePageComponent } from './Components/welcome-page/welcome-page.component';
+import { AboutUsComponent } from './Components/about-us/about-us.component';
 
-const routes: Routes = [
-  { path: '', component: LoginComponent },
-  { path: 'register', component: RegisterComponent },
+const routes: Routes = 
+[
+  { path: '', component: WelcomePageComponent },
+  { path: 'login', component: LoginComponent },
   // for adding new place
   { path: 'add-place', component: ContinuedRegisterComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'location', component: LocationPageComponent },
-  { path: 'home', component: HomeFiltrationContainerComponent },
   { path: 'place-details/:placeId', component: PlaceDetailsComponent },
   { path: 'notification', component: InvitationComponent },
-  { path: 'user-profile', component: UserProfileComponent }
+  { path: 'user-profile', component: UserProfileComponent },
+  { path: 'places', component: HomeFiltrationContainerComponent },
+  { path: 'about-us', component: AboutUsComponent }
 
-];
+]
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
