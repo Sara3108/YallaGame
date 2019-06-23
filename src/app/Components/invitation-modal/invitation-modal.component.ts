09f6afd7fd2d8b88251554e306f084dbf61bdc3a
Sara3108 <51201757+Sara3108@users.noreply.github.com>
2019-06-23 18:55:14 +0200

    invitation service

--- src/app/Components/invitation-modal/invitation-modal.component.ts
+++ src/app/Components/invitation-modal/invitation-modal.component.ts
@@ -22,6 +22,10 @@ export class InvitationModalComponent implements OnInit {
       console.log("ONLINE USERS   "+this.onlineUsers);
     })
   }
+  f(){
+    console.log("ONLINE USERS   "+this.onlineUsers);
+
+  }
 
 
 
